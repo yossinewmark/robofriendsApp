8ef6fed85603821b14220c0a5248a0cb943acc18
yossinewmark <yossi.newmark@gmail.com>
2020-07-20 13:47:31 -0400

    robofriends app in react practice

---
+++ src/components/Scroll.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    console.log(props)
+    return (
+        <div style={{overflowY: 'scroll', border: '1px solid black', height: '1000px' }}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Scroll;
