54a32b493543aed894702792fc934aa8889892dd
yossinewmark <yossi.newmark@gmail.com>
2020-07-20 15:42:50 -0400

    corrected spelling of Error Boundary Component

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class ErrorBoundry extends Component {
+class ErrorBoundary extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -20,4 +20,4 @@ class ErrorBoundry extends Component {
 
 }
 
-export default ErrorBoundry;
+export default ErrorBoundary;
