595b3348856c43d21bbb0936c267a98f42b2a981
yossinewmark <yossi.newmark@gmail.com>
2020-07-20 16:14:40 -0400

    Updates

---
+++ precache-manifest.25269b4251eca49a758eb8ce0e55312c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3d47467d8dee1db4509c796be2debf",
+    "url": "/robofriendsApp/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriendsApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "767ca1ad6a9f23a0a997",
+    "url": "/robofriendsApp/static/css/main.5275a630.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriendsApp/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriendsApp/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767ca1ad6a9f23a0a997",
+    "url": "/robofriendsApp/static/js/main.47f1cc1b.chunk.js"
+  },
+  {
+    "revision": "37070ec647620d372c20",
+    "url": "/robofriendsApp/static/js/runtime-main.73db2982.js"
+  }
+]);
