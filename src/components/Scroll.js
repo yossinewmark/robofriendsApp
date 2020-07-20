971b518c47875d379a38157808e7f61a06e357cd
yossinewmark <yossi.newmark@gmail.com>
2020-07-20 14:05:18 -0400

    cleaned up console logging from components

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Scroll = (props) => {
-    console.log(props)
     return (
         <div style={{overflowY: 'scroll', border: '1px solid black', height: '1000px' }}>
             {props.children}
